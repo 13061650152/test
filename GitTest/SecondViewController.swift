cc78f71e7d9b5d47335df80d37003c7f03b690ff
赵哈哈 <zyc@zhaohahadeMacBook-Pro.local>
2017-09-03 22:03:28 +0800

    hello world

---
+++ GitTest/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  GitTest
+//
+//  Created by 赵哈哈 on 2017/9/3.
+//  Copyright © 2017年 赵哈哈. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: ViewController {
+
+    var label = UILabel();
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        
+        
+    }
+}
--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView = UITableView();
+    var tf = UITextField();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
